291e682b4881a977725a35095fa8ae33e02f1b16
nytouu <capitaine7312@gmail.com>
2024-11-12 15:03:32 +0100

    Add Door Particles

--- Assets/Scripts/Interactible/Door.cs
+++ Assets/Scripts/Interactible/Door.cs
@@ -2,23 +2,48 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-	[SerializeField] private Animator animator;
-	[SerializeField] private Transform player;
-	[SerializeField] private Transform door;
-	[SerializeField] private float distanceToActivate;
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private Transform door;
+    [SerializeField]
+    private ParticleSystem particles;
+    [SerializeField]
+    [Range(1f, 25f)]
+    private float distanceToActivate;
+
+    [SerializeField]
+    private bool particlesEnabled;
+
+    void Start()
+    {
+        particlesEnabled = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
-		float distance = Vector3.Distance(player.position, door.position);
+        float distance = Vector3.Distance(player.position, door.position);
 
-		if (distance <= distanceToActivate)
+        if (distance <= distanceToActivate)
+        {
+            animator.SetBool("Should Open", true);
+        }
+        else
+        {
+            animator.SetBool("Should Open", false);
+        }
+
+		// particlesEnabled is controlled by the animation clip.
+		if (particlesEnabled && !particles.isPlaying)
 		{
-		    animator.SetBool("Should Open", true);
+			particles.Play();
 		}
-		else
+		else if (!particlesEnabled && particles.isPlaying)
 		{
-		    animator.SetBool("Should Open", false);
+			particles.Stop();
 		}
     }
 }
